6a65577089517d9b77570d1b2c8281c80258b898
kouta1612 <kouta1612world69@gmail.com>
2023-01-10 08:49:58 +0900

    蒸散プログラム実装

--- hardware/nand2tetris/projects/04/mult/Mult.asm
+++ hardware/nand2tetris/projects/04/mult/Mult.asm
@@ -10,3 +10,33 @@
 // R0 >= 0, R1 >= 0, and R0*R1 < 32768.
 
 // Put your code here.
+@0
+D=M // D = R0
+@i
+M=D // i = D
+@1
+D=M // D = R1
+@j
+M=D // j = D
+@mul
+M=0 // mul = 0
+(LOOP)
+@i
+D=M // D = i
+@END
+D;JLE // if D <= 0 then goto END
+@j
+D=M // D = j
+@mul
+M=M+D // mul += D
+@i
+M=M-1 // i--
+@LOOP
+0;JMP // goto LOOP
+(END)
+@mul
+D=M // D = mul
+@2
+M=D // R2 = D
+@END
+0;JMP // goto END (infinite)
