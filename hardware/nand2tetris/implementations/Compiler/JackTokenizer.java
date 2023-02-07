c86474e68f039f47a2623d85ad3f679861a6d08a
kouta1612 <kouta1612world69@gmail.com>
2023-02-07 23:49:05 +0900

    tokenizer api実装進めた

--- hardware/nand2tetris/implementations/Compiler/JackTokenizer.java
+++ hardware/nand2tetris/implementations/Compiler/JackTokenizer.java
@@ -3,14 +3,18 @@ package implementations.Compiler;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class JackTokenizer {
-    private static final String WORD_PATTERN = "[a-zA-Z0-9]";
-    private static final String SYMBOL_PATTERN = "[{}()\\[\\].,;+\\-*/&|<>=~]";
+    private static final String WORD_PATTERN = "^[a-zA-Z0-9]$";
+    private static final String SYMBOL_PATTERN = "^[{}()\\[\\].,;+\\-*/&|<>=~]$";
+    private static final String KEYWORD_PATTERN = "^class|constructor|function|method|field|static|var|int|char|boolean|void|true|false|null|this|let|do|if|else|while|return$";
+    private static final String INT_PATTERN = "^\\d+$";
+    private static final String STRING_PATTERN = "^\"[^\"\n]*\"$";
+    private static final String IDENTIFIER_PATTERN = "^[^\\d][a-zA-z0-9_]*$";
+
     private static final Map<Character, String> SYMBOL_REPLACE_MAP = new HashMap<>() {
         {
             put('<', "&lt;");
@@ -21,6 +25,10 @@ public class JackTokenizer {
 
     private static BufferedReader bufferedReader;
     private static List<String> tokens;
+    private static int tokenIndex = 0;
+    private static String currentToken;
+    private static String currentTokenType;
+    // private static List<String> xmlTokens;
 
     JackTokenizer(File file) throws Exception {
         FileReader fileReader = new FileReader(file);
@@ -28,8 +36,36 @@ public class JackTokenizer {
         addTokens();
     }
 
-    public void advance() {
+    public boolean hasMoreTokens() {
+        return tokens.size() > tokenIndex;
+    }
 
+    public void advance() {
+        String token = tokens.get(tokenIndex);
+        if (token.equals(KEYWORD_PATTERN)) {
+            currentToken = "<keyword> " + token + " </keyword>";
+            currentTokenType = "KEYWORD";
+        } else if (token.equals(SYMBOL_PATTERN)) {
+            currentToken = "<symbol> " + token + " </symbol>";
+            currentTokenType = "SYMBOL";
+        } else if (token.equals(IDENTIFIER_PATTERN)) {
+            currentToken = "<identifier> " + token + " </identifier>";
+            currentTokenType = "IDENTIFIER";
+        } else if (token.equals(INT_PATTERN)) {
+            if (!(0 <= Integer.valueOf(token) && Integer.valueOf(token) <= 32767)) {
+                throw new IllegalArgumentException("integerConstantの値は0から32767までの数字です");
+            }
+            currentToken = "<integerConstant> " + token + "\n</integerConstant>";
+            currentTokenType = "INT_CONST";
+        } else if (token.equals(STRING_PATTERN)) {
+            if (token.length() > 2) {
+                token = token.substring(1, token.length() - 2);
+            } else {
+                token = "";
+            }
+            currentToken = "<stringConstant> " + token + "\n</stringConstant>";
+            currentTokenType = "STRING_CONST";
+        }
     }
 
     private void addTokens() throws Exception {
@@ -41,17 +77,18 @@ public class JackTokenizer {
             if (readLine.equals("")) {
                 continue;
             }
-            // System.out.println(readLine);
-            List<String> tests = getTokens(readLine);
-            for (String test : tests) {
-                System.out.println(test);
-            }
+            setTokens(readLine);
         }
         bufferedReader.close();
     }
 
-    private List<String> getTokens(String line) throws Exception {
-        List<String> result = new ArrayList<>();
+    // private void lexicalAnalyze() throws Exception {
+    // for (String token : tokens) {
+
+    // }
+    // }
+
+    private void setTokens(String line) throws Exception {
         char[] cs = line.toCharArray();
         StringBuilder tokenBuilder = new StringBuilder();
 
@@ -60,38 +97,31 @@ public class JackTokenizer {
                 tokenBuilder.append(cs[i]);
             } else if (Character.toString(cs[i]).equals(" ")) {
                 if (!tokenBuilder.toString().equals("")) {
-                    result.add(tokenBuilder.toString());
+                    tokens.add(tokenBuilder.toString());
                     tokenBuilder.setLength(0);
                 }
             } else if (Character.toString(cs[i]).matches(SYMBOL_PATTERN)) {
-                if (cs[i] == '<' || cs[i] == '>' || cs[i] == '&') {
-                    tokenBuilder.append(SYMBOL_REPLACE_MAP.get(cs[i]));
-                } else if (cs[i] == ';' || cs[i] == '{' || cs[i] == '}' || cs[i] == '(' || cs[i] == ')'
-                        || cs[i] == ',') {
-                    if (!tokenBuilder.toString().equals("")) {
-                        result.add(tokenBuilder.toString());
-                    }
-                    result.add(Character.toString(cs[i]));
-                    tokenBuilder.setLength(0);
-                } else {
-                    tokenBuilder.append(cs[i]);
+                if (!tokenBuilder.toString().equals("")) {
+                    tokens.add(tokenBuilder.toString());
                 }
+                tokens.add(Character.toString(cs[i]));
+                tokenBuilder.setLength(0);
             } else if (i == cs.length - 1 && !tokenBuilder.toString().equals("")) {
-                result.add(tokenBuilder.toString());
+                tokens.add(tokenBuilder.toString());
                 tokenBuilder.setLength(0);
             } else if (cs[i] == '"') {
+                tokenBuilder.append(cs[i]);
                 i++;
                 while (cs[i] != '"') {
                     tokenBuilder.append(cs[i]);
                     i++;
                 }
-                result.add(tokenBuilder.toString());
+                tokenBuilder.append(cs[i]);
+                tokens.add(tokenBuilder.toString());
                 tokenBuilder.setLength(0);
             } else {
-                // throw new Exception("kota error.");
+                throw new Exception("トークン取得時に予期せぬエラーが発生しました。");
             }
         }
-
-        return result;
     }
 }
--- hardware/nand2tetris/implementations/Compiler/Main.java
+++ hardware/nand2tetris/implementations/Compiler/Main.java
@@ -1,10 +1,26 @@
 package implementations.Compiler;
 
 import java.io.File;
+import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws Exception {
         File file = new File(args[0]);
-        JackTokenizer tokenizer = new JackTokenizer(file);
+        if (args.length == 0) {
+            throw new IllegalAccessException("引数にパスを指定してください");
+        }
+        if (!file.exists()) {
+            throw new IOException("指定したパスは見つかりませんでした");
+        }
+
+        if (file.isDirectory()) {
+            //
+        } else {
+            if (!file.getName().matches(".+\\.jack")) {
+                throw new IllegalAccessException("jackファイルを指定してください");
+            }
+            JackTokenizer tokenizer = new JackTokenizer(file);
+
+        }
     }
 }
